b2e093ccacf4d228100db2a1b549ad9fac4db36a
Isaac Ephrem <68367687+ije2c@users.noreply.github.com>
2020-12-11 08:23:02 -0600

    Update Temp.py

--- Temperature Conversion Program/Temp.py
+++ Temperature Conversion Program/Temp.py
@@ -2,6 +2,6 @@
 
 celsius = int(raw_input("Enter a temperature in Celsius: "))
  
-farenheit_temp = 9.0/5.0 * Celsius + 32
+farenheit_temp = 9.0/5.0 * celsius + 32
  
-print "Celsius Temperature to Convert:", Celsius, "Celsius = ", farenheit_temp, " F"
+print "Celsius Temperature to Convert:", celsius, "Celsius = ", farenheit_temp, " F"
